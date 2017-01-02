ba24e7be8ff052cb4b6987bb4e0f7a580f58bb28
tim.lambert <tlambert@gmail.com>
2017-01-02 17:57:13 +0000

    Initial commit

---
+++ WpfTest/Extensions/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTest.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToNameFormat(this int ordinal)
+        {
+            var stringOrdinal = ordinal.ToString();
+            return stringOrdinal.StartsWith("-") 
+                ? string.Concat("m", stringOrdinal.Substring(1, stringOrdinal.Length - 1)) 
+                : string.Concat("p", stringOrdinal);
+        }
+
+        public static int ToAbsoluteColor(this int color)
+        {
+            return color < 0 ? color * -1 : color;
+        }
+    }
+}
---
+++ WpfTest/Models/ColorShifter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Ink;
+using System.Windows.Media;
+using WpfTest.Extensions;
+
+namespace WpfTest.Models
+{
+    public class ColorShifter
+    {
+        private int _r;
+        private int _g;
+        private int _b;
+
+        public int Red { get { return _r; } }
+        public int Green { get { return _g; } }
+        public int Blue { get { return _b; } }
+
+        public Color ColorFromRgb
+        {
+            get
+            {
+                var r = _r.ToAbsoluteColor();
+                var g = _g.ToAbsoluteColor();
+                var b = _b.ToAbsoluteColor();
+                return Color.FromRgb((byte)r, (byte)g, (byte)b);
+            }
+        }
+
+        public ColorShifter(int r, int g, int b)
+        {
+            _r = r;
+            _g = g;
+            _b = b;
+        }
+
+        public void Increment(int step = 1)
+        {
+            _r = Increment(_r, step);
+            _g = Increment(_g, step);
+            _b = Increment(_b, step);
+        }
+
+        public ColorShifter Clone()
+        {
+            return new ColorShifter(_r, _g, _b);
+        }
+
+        private int Increment(int value, int step)
+        {
+            value = value + step;
+            return value > 255 ? value - 510 : value;
+        }
+    }
+}
